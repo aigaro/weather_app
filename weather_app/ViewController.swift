ed2d313e15af45d794e2fe84d43779f636c9228b
Apple Inc <aigar@aigars-mac-pro.lan>
2017-02-19 22:25:20 +0200

    start

--- weather_app/ViewController.swift
+++ weather_app/ViewController.swift
@@ -8,18 +8,53 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, XMLParserDelegate {
+    
+    func beginParse() {
 
+    var ilmastik: String = String()
+    let urlString = NSURL(string: "https://www.ilmateenistus.ee/ilma_andmed/xml/forecast.php")
+        let xmlWeather = XMLParser(contentsOf: urlString as! URL)
+        xmlWeather?.delegate = self
+        xmlWeather?.parse()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //Degrees.text = temperature
+        //Degrees.text = ilmastik
+        
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
+    func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
+        
+        
+        if elementName == "phenomenon" {
+            
+        }
+        
+    }
+    
+    func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+       
+    }
+    
+    func parser(_ parser: XMLParser, foundCharacters string: String) {
+       
+        
+    }
+    
+    
+    
+    
+    @IBOutlet weak var Degrees: UILabel!
+    
+    
+    
+}
